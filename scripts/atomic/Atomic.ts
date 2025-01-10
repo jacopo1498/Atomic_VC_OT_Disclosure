bff90bd9d766b9d630b856449fa42cab6c84be5b
Jacopo1498 <115628265+jacopo1498@users.noreply.github.com>
2025-01-10 17:01:50 +0000

    Update Atomic.ts

--- scripts/atomic/Atomic.ts
+++ scripts/atomic/Atomic.ts
@@ -55,24 +55,6 @@ export function getMultipleRandom(arr: string[], num: number) {
   return shuffled.slice(0, num);
 }
 
-export async function issueVP(jwtSet: string[],disclosedClaims: number, subject: EthrDID){
-	let disclosedJWTset = getMultipleRandom(jwtSet, disclosedClaims);
-    console.log("\x1b[43m","the number of claims (vc) disclosed is :"+disclosedClaims,'\x1b[0m');
-	console.log(disclosedJWTset);
-	const VPPayload = createVPPayload(disclosedJWTset);
-	let jwtVP = await createVerifiablePresentationJwt(VPPayload,subject as Issuer ,options as {});
-    console.log("\x1b[43m","verifiable presentation issued:",'\x1b[0m');
-    console.log(jwtVP)
-	return jwtVP;
-}
-
-export async function verifyVP(jwtVP: string,didResolver: Resolver){
-	const verifiedPresentation= await verifyPresentation(jwtVP, didResolver,{});	
-    console.log("\x1b[44m","verified presentation:",'\x1b[0m');
-    console.log(verifiedPresentation)
-	return;
-}
-
 //qui riempiamo le VC di attributi
 async function createVCPayload(user: EthrDID,nClaims: number) {
 	let atomicVC=[];
@@ -80,7 +62,7 @@ async function createVCPayload(user: EthrDID,nClaims: number) {
    		var attrName="attrName"+i;
 		var attrValue="attrValue"+i;
 		const VCPayload={} as JwtCredentialPayload;
-		VCPayload['sub']=user.did; //perchè era commentato?
+		VCPayload['sub']=user.did;
     	//VCPayload['nbf']=626105238;
     	VCPayload['vc']= {
 			'@context': ['https://www.w3.org/2018/credentials/v1'],
@@ -95,13 +77,5 @@ async function createVCPayload(user: EthrDID,nClaims: number) {
 }
 
 
-function createVPPayload(vc: string[]) {
-	const VPPayload={} as JwtPresentationPayload;
-    VPPayload['vp']= {
-			'@context': ['https://www.w3.org/2018/credentials/v1'],
-			type: ['VerifiablePresentation'],
-			verifiableP: vc
-		};
-	return VPPayload;
-}
+
 
